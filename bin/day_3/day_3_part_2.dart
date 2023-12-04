4c5dafdc4195ce1c6a4404734bdf679d5a5a6c98
andrehaueisen <andrehaueisen@gmail.com>
2023-12-04 06:56:57 -0300

    Started solving day_3_part_2.dart

---
+++ bin/day_3/day_3_part_2.dart
@@ -0,0 +1,105 @@
+import 'package:meta/meta.dart';
+
+import '../utils.dart';
+
+void main(List<String> arguments) async {
+  final schematicLines = await loadStringsFromFile('assets/engine_schematic.txt');
+  final totalLines = schematicLines.length;
+
+  final allGearRatios = <int>[];
+  for (int index = 0; index < schematicLines.length; index++) {
+    final line = schematicLines[index];
+    final starMatches = findStars(line);
+
+    final gearRatios = starMatches
+        .map(
+          (match) {
+            return getTwoAdjacentNumbers(
+              currentLine: line,
+              previousLine: index > 0 ? schematicLines[index - 1] : null,
+              nextLine: index < totalLines - 1 ? schematicLines[index + 1] : null,
+              match: match,
+            );
+          },
+        )
+        .where((adjacentNumbers) => adjacentNumbers != null)
+        .map((gearNumbers) => gearNumbers!.$1 * gearNumbers.$2);
+
+    allGearRatios.addAll(gearRatios);
+  }
+
+  final sumOfRatios = allGearRatios.fold(0, (previousValue, element) => previousValue + element);
+}
+
+@visibleForTesting
+(int firstNumber, int secondNumber)? getTwoAdjacentNumbers({
+  required String currentLine,
+  required String? previousLine,
+  required String? nextLine,
+  required RegExpMatch match,
+}) {
+  final currentLineLength = currentLine.length;
+
+  List<int> adjacentNumbers = [];
+  if (previousLine != null) {
+    final matches = adjacentMatchesInLine(
+      lineUnderInvestigation: previousLine,
+      match: match,
+      starLineLength: currentLineLength,
+    );
+    if (matches.length > 2) return null;
+
+    final numbers = matches.map((match) => int.parse(match.group(0)!));
+    adjacentNumbers.addAll(numbers);
+  }
+
+  if (nextLine != null) {
+    final matches = adjacentMatchesInLine(
+      lineUnderInvestigation: nextLine,
+      match: match,
+      starLineLength: currentLineLength,
+    );
+    if (matches.length > 2) return null;
+
+    final numbers = matches.map((match) => int.parse(match.group(0)!));
+    adjacentNumbers.addAll(numbers);
+  }
+
+  final matches = adjacentMatchesInLine(
+    lineUnderInvestigation: currentLine,
+    match: match,
+    starLineLength: currentLineLength,
+  );
+
+  final numbers = matches.map((match) => int.parse(match.group(0)!));
+  adjacentNumbers.addAll(numbers);
+
+  if (adjacentNumbers.length != 2) return null;
+  return (adjacentNumbers[0], adjacentNumbers[1]);
+}
+
+@visibleForTesting
+List<RegExpMatch> adjacentMatchesInLine({
+  required String lineUnderInvestigation,
+  required RegExpMatch match,
+  required int starLineLength,
+}) {
+  final matchIndex = match.start;
+
+  final numbersMatches = _findNumbers(lineUnderInvestigation);
+
+  final numbers = numbersMatches.where((numberMatch) {
+    print('line: $lineUnderInvestigation, numberMatch: ${numberMatch.group(0)}');
+    print('numberMatch.end: ${numberMatch.end}, numberMatch.start: ${numberMatch.start}, matchIndex: $matchIndex');
+    final isBefore = numberMatch.end >= matchIndex - 1;
+    final isAfter = numberMatch.start == matchIndex + 1;
+    final isAtTheSamePosition = numberMatch.start == matchIndex;
+    return isBefore || isAfter || isAtTheSamePosition;
+  }).toList(growable: false);
+
+  return numbers;
+}
+
+@visibleForTesting
+List<RegExpMatch> findStars(String line) => RegExp(r'\*').allMatches(line).toList(growable: false);
+List<RegExpMatch> _findNumbers(String line) => RegExp(r'\d+').allMatches(line).toList(growable: false);
---
+++ test/day_3/day_3_part_2.dart
@@ -0,0 +1,199 @@
+import 'package:test/test.dart';
+
+import '../../bin/day_3/day_3_part_2.dart';
+
+void main() {
+  //.....786....
+  //.......*....
+  //....615.....
+  test('adjacentMatchesInLine - one', () {
+    final starMatches = findStars('.......*....');
+
+    final firstLineResults = adjacentMatchesInLine(
+      lineUnderInvestigation: '....786.....',
+      match: starMatches.first,
+      starLineLength: 12,
+    );
+
+    expect(firstLineResults.length, 1);
+    expect(firstLineResults.first.group(0), '786');
+
+    final secondLineResults = adjacentMatchesInLine(
+      lineUnderInvestigation: '.......*....',
+      match: starMatches.first,
+      starLineLength: 12,
+    );
+
+    expect(secondLineResults.length, 0);
+
+    final thirdLineResults = adjacentMatchesInLine(
+      lineUnderInvestigation: '....615.....',
+      match: starMatches.first,
+      starLineLength: 12,
+    );
+
+    expect(thirdLineResults.length, 1);
+    expect(thirdLineResults.first.group(0), '615');
+  });
+
+  //.....786....
+  //.......*....
+  //....615.....
+  test('getTwoAdjacentNumbers - one', () {
+    (int firstNumber, int secondNumber)? results = getTwoAdjacentNumbers(
+      currentLine: '.......*....',
+      previousLine: '....786.....',
+      nextLine: '....615.....',
+      match: findStars('.......*....').first,
+    );
+
+    expect(results?.$1, 786);
+    expect(results?.$2, 615);
+  });
+
+  //.....78.9...
+  //.......*3...
+  //....61......
+  test('adjacentMatchesInLine - two', () {
+    final starMatches = findStars('.......*3...');
+
+    final firstLineResults = adjacentMatchesInLine(
+      lineUnderInvestigation: '.....78.9...',
+      match: starMatches.first,
+      starLineLength: 12,
+    );
+
+    expect(firstLineResults.length, 2);
+    expect(firstLineResults.first.group(0), '78');
+    expect(firstLineResults.last.group(0), '9');
+
+    final secondLineResults = adjacentMatchesInLine(
+      lineUnderInvestigation: '.......*3...',
+      match: starMatches.first,
+      starLineLength: 12,
+    );
+
+    expect(secondLineResults.length, 1);
+    expect(secondLineResults.first.group(0), '3');
+
+    final thirdLineResults = adjacentMatchesInLine(
+      lineUnderInvestigation: '....61......',
+      match: starMatches.first,
+      starLineLength: 12,
+    );
+
+    expect(thirdLineResults.length, 0);
+  });
+
+  //.....78.9...
+  //.......*3...
+  //....61......
+  test('getTwoAdjacentNumbers - two', () {
+    (int firstNumber, int secondNumber)? results = getTwoAdjacentNumbers(
+      currentLine: '.......*3...',
+      previousLine: '.....78.9...',
+      nextLine: '....61......',
+      match: findStars('.......*3...').first,
+    );
+
+    expect(results, isNull);
+  });
+
+  //.....78.9...
+  //......1*3...
+  //....61.29...
+  test('adjacentMatchesInLine - three', () {
+    final starMatches = findStars('......1*3...');
+
+    final firstLineResults = adjacentMatchesInLine(
+      lineUnderInvestigation: '.....78.9...',
+      match: starMatches.first,
+      starLineLength: 12,
+    );
+
+    expect(firstLineResults.length, 2);
+    expect(firstLineResults.first.group(0), '78');
+    expect(firstLineResults.last.group(0), '9');
+
+    final secondLineResults = adjacentMatchesInLine(
+      lineUnderInvestigation: '......1*3...',
+      match: starMatches.first,
+      starLineLength: 12,
+    );
+
+    expect(secondLineResults.length, 2);
+    expect(secondLineResults.first.group(0), '1');
+    expect(secondLineResults.last.group(0), '3');
+
+    final thirdLineResults = adjacentMatchesInLine(
+      lineUnderInvestigation: '....61.29...',
+      match: starMatches.first,
+      starLineLength: 12,
+    );
+
+    expect(thirdLineResults.length, 1);
+    expect(thirdLineResults.first.group(0), '29');
+  });
+
+  //.....78.9...
+  //......1*3...
+  //....61.29...
+  test('getTwoAdjacentNumbers - three', () {
+    (int firstNumber, int secondNumber)? results = getTwoAdjacentNumbers(
+      currentLine: '......1*3...',
+      previousLine: '.....78.9...',
+      nextLine: '....61.29...',
+      match: findStars('......1*3...').first,
+    );
+
+    expect(results, isNull);
+  });
+
+  //......780...
+  //.......*....
+  //......029...
+  test('adjacentMatchesInLine - four', () {
+    final starMatches = findStars('.......*....');
+
+    final firstLineResults = adjacentMatchesInLine(
+      lineUnderInvestigation: '......780...',
+      match: starMatches.first,
+      starLineLength: 12,
+    );
+
+    expect(firstLineResults.length, 1);
+    expect(firstLineResults.first.group(0), '780');
+
+    final secondLineResults = adjacentMatchesInLine(
+      lineUnderInvestigation: '.......*....',
+      match: starMatches.first,
+      starLineLength: 12,
+    );
+
+    expect(secondLineResults.length, 0);
+
+    final thirdLineResults = adjacentMatchesInLine(
+      lineUnderInvestigation: '......029...',
+      match: starMatches.first,
+      starLineLength: 12,
+    );
+
+    expect(thirdLineResults.length, 1);
+    expect(thirdLineResults.first.group(0), '029');
+  });
+
+  //......780...
+  //.......*....
+  //......029...
+  test('getTwoAdjacentNumbers - four', () {
+    (int firstNumber, int secondNumber)? results = getTwoAdjacentNumbers(
+      currentLine: '.......*....',
+      previousLine: '......780...',
+      nextLine: '......029...',
+      match: findStars('.......*....').first,
+    );
+
+    expect(results?.$1, 780);
+    expect(results?.$2, 029);
+  });
+}
